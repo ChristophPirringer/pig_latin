e4e3d0660168f1fd68b52bd4debfdb050b0e7561
Christoph Pirringer & Austin Kincaid <student@epicodus.com>
2015-09-09 11:13:14 -0700

    add frontend to this wonderful app

--- js/scripts.js
+++ js/scripts.js
@@ -7,19 +7,18 @@ var pig_latin = function(input) {
   var return_latin = [];
 
   raw_input.forEach(function(word) {
-    var vowel_counter = -1;
+    var consonant_counter = -1;
 
     do{
-      vowel_counter +=1;
-      if ( word.charAt(vowel_counter) === "q" && word.charAt(vowel_counter+1) === "u") {
-        vowel_counter +=2;
+      consonant_counter +=1;
+      if ( word.charAt(consonant_counter) === "q" && word.charAt(consonant_counter+1) === "u") {
+        consonant_counter +=2;
       }
-    } while(vowels.indexOf(word.charAt(vowel_counter))===-1 );
+    } while(vowels.indexOf(word.charAt(consonant_counter))===-1 );
 
-    var vowel_beginning = word.slice(0,vowel_counter);
-    var input = word.replace(vowel_beginning,"");
-    input = input.concat(vowel_beginning);
-  // debugger;
+    var consonant_beginning = word.slice(0,consonant_counter);
+    var input = word.replace(consonant_beginning,"");
+    input = input.concat(consonant_beginning);
 
     if(vowels.indexOf(input.charAt(0))===-1 && input.charAt(0)!=="y" ){
       output = input;
@@ -31,3 +30,16 @@ var pig_latin = function(input) {
   });
   return return_latin.join(" ");
 };
+
+
+$(document).ready(function() {
+  $("form#pig_latin").submit(function(event) {
+    var raw_input = $("input#raw_input").val();
+
+    var result = pig_latin(raw_input);
+
+    $(".output").text(result);
+
+    event.preventDefault();
+  });
+});
