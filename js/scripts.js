780ec7cefb9d3edb119caeae84e889aa967a2a8d
Christoph Pirringer & Austin Kincaid <student@epicodus.com>
2015-09-09 10:58:12 -0700

    Add spec and methods to handle multiple words

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,33 @@
 var pig_latin = function(input) {
   var raw_input = input.toLowerCase();
   var output = "";
-  var consonants = ["a","e","i","o","u"];
+  var vowels = ["a","e","i","o","u"];
 
-  var consonant_counter = -1;
+  var raw_input = raw_input.split(" ");
+  var return_latin = [];
 
-  do{
-      consonant_counter +=1;
-      if ( raw_input.charAt(consonant_counter) === "q" && raw_input.charAt(consonant_counter+1) === "u") {
-        consonant_counter +=2;
+  raw_input.forEach(function(word) {
+    var vowel_counter = -1;
+
+    do{
+      vowel_counter +=1;
+      if ( word.charAt(vowel_counter) === "q" && word.charAt(vowel_counter+1) === "u") {
+        vowel_counter +=2;
       }
-    } while(consonants.indexOf(raw_input.charAt(consonant_counter))==-1 );
+    } while(vowels.indexOf(word.charAt(vowel_counter))===-1 );
 
-  var consonant_beginning = raw_input.slice(0,consonant_counter);
-  var input = raw_input.replace(consonant_beginning,"");
-  input = input.concat(consonant_beginning);
-// debugger;
+    var vowel_beginning = word.slice(0,vowel_counter);
+    var input = word.replace(vowel_beginning,"");
+    input = input.concat(vowel_beginning);
+  // debugger;
 
-  if(consonants.indexOf(input.charAt(0))==-1){
-    output = input;
-  }else{
-    output = input;
-    output = output.concat("ay");
-  };
-  return output;
+    if(vowels.indexOf(input.charAt(0))===-1 && input.charAt(0)!=="y" ){
+      output = input;
+    }else{
+      output = input;
+      output = output.concat("ay");
+    };
+    return_latin.push(output);
+  });
+  return return_latin.join(" ");
 };
--- spec/specs.js
+++ spec/specs.js
@@ -10,4 +10,12 @@ describe('pig_latin', function() {
   it("returns 'ealsquay' for the word 'squeal'", function() {
     expect(pig_latin("squeal")).to.equal("ealsquay");
   });
+
+  it("returns 'ellowyay' for the word 'yellow'", function() {
+    expect(pig_latin("yellow")).to.equal("ellowyay");
+  });
+
+  it("returns 'ellowyay eggay' for the sentence 'yellow egg'", function() {
+    expect(pig_latin("yellow egg")).to.equal("ellowyay eggay");
+  });
 });
